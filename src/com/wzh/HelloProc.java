517dd59a220fbca32df26e897c4ffdec8be50f12
JohnSmith20220422 <13915370347@163.com>
2022-04-22 12:00:19 +0800

    test1

---
+++ src/com/wzh/HelloProc.java
@@ -0,0 +1,5 @@
+package com.wzh;
+
+public interface HelloProc {
+    public String print();
+}
---
+++ src/com/wzh/Main.java
@@ -0,0 +1,10 @@
+package com.wzh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello wold !!!!");
+        System.out.println("Hello wold !!!!2222");
+    }
+}
